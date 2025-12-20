91baab9ac5281844d9d8c537c1ed95e2dacf93e4
Christian <christian.acu.95@gmail.com>
2025-12-20 19:37:41 -0300

    Cambios en /Contactanos

--- src/app/pages/contactanos/contactanos.ts
+++ src/app/pages/contactanos/contactanos.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 import { FormContactanos } from '@/app/components/pages/contactanos/form-contactanos/form-contactanos';
-import { HeroContactanos } from '@/app/components/pages/contactanos/hero-contactanos/hero-contactanos';
 
 @Component({
   selector: 'app-contactanos',
-  imports: [HeroContactanos, FormContactanos],
+  imports: [FormContactanos],
   templateUrl: './contactanos.html',
   styleUrl: './contactanos.css',
 })
